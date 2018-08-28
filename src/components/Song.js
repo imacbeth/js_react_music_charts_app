920e2ac91d88e0cc2b80229cad34675ab4316d2f
Iona Macbeth <ionalmacbeth@gmail.com>
2018-08-28 17:07:47 +0100

    hw completed

--- src/components/Song.js
+++ src/components/Song.js
@@ -2,12 +2,11 @@ import React from 'react';
 
 const Song = function (props) {
   if (!props) return null;
-  console.log(props);
+
   return (
     <div>
-    <h3>{props.position}: {props.title.label}</h3>
-    <h3>{props.artist.label}</h3>
-    <img src={props.image[2].label} alt=""></img>
+    <h3>{props.position}. {props.title.label} - {props.artist.label}</h3>
+    <img src={props.image[2].label} alt="single artwork"></img>
   </div>
   );
 }
